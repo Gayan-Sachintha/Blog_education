2d80b4b9b39de34a0556f88f7532ec5b0dc816d9
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-02 12:16:38 +0530

    Create Nav.jsx

---
+++ src/pages/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../css/Style.css'
+
+const Nav = () => {
+
+    return (
+
+        <div className='Navigation'>
+
+            {/* Nav-bar start */}
+
+            <nav id='nav'>
+
+                <div className="logo">
+                    <span>Logo</span>
+                </div>
+
+
+
+            </nav>
+
+            {/* Nav-bar End */}
+
+
+        </div>
+    )
+}
+
+export default Nav;
