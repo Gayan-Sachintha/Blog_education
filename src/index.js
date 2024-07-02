916853db8060752d88a40c92bce8661edc7a3a56
Pasindu Peiris <it21831454@my.sliit.lk>
2024-07-02 11:31:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const index = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
 
-export default index
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
